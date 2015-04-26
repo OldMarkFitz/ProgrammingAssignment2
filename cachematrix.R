8adebb1e741efe7343d9e492af04a8a93d7f47a0
Fitz <email4fitz@gmail.com>
2015-04-26 09:55:08 -0400

    For Coursera - Fitz

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions will return the inverse of a supplied matrix, however 
+## if the same matrix is passed as an input parameter, it will return
+## the cached result( i.e will not recalculate the result).
 
-## Write a short comment describing this function
+## This function sets and gets the inverse matrix and will cache it
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <-NULL
+        set <- function(y){
+            x <<- y
+            m <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) m <<- inverse
+        getInverse <- function() m
+        list(get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function will get the cached inverse matrix or calculate
+## it if it's the first time
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        t <- x$getInverse()
+        if (!is.null(t)) {
+            message("getting cached data")
+            return(t)
+        }
+        mat <- x$get()
+        t <- solve(mat)
+        x$setInverse(t)
+        t
 }
